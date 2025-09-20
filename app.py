499dce6d76655598d650b13ea3e68ff46bf372d0
devopsworkshop2025 <devopsworkshop2025@gmail.com>
2025-09-20 11:07:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask Dockerized!--> updated code'
+    return 'Hello from Flask Dockerized!--Pooja'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
